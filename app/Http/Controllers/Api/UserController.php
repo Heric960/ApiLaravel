4d9f87637f6b63314e43b2305f4dd7f24cf929eb
heric <hericalves960@gmail.com>
2024-12-13 14:56:14 -0300

    finalizando o projeto

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
@@ -20,17 +20,16 @@ class UserController extends Controller
      * 
      * @return \illuminate\Http\JsonResponse
      */
-    public function index() : JsonResponse
+    public function index(): JsonResponse
     {
         // Recupera os usuários do banco de dados, ordenados pelo id em ordem decrescente, paginados
         $users = User::orderBy('id', 'DESC')->paginate(2);
 
         // Retorna os usuários recuperados como uma resposta JSON
-        return response() -> json([
+        return response()->json([
             'starus' => true,
             'users' => $users,
         ], 200);
-
     }
 
     /**
@@ -41,23 +40,28 @@ class UserController extends Controller
      * @param \App\Models\User $user O objeto do usuário a ser exibido
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show(User $user) : JsonResponse
+    public function show(User $user): JsonResponse
     {
         // Retorna os detalhes do usuário em fomato JSON
-        return response() -> json([
+        return response()->json([
             'starus' => true,
             'user' => $user,
         ], 200);
-
     }
 
-    public function store(Request $request)
+
+    /**
+     * Cria novo usuário com os dados fornecidos na rquisição.
+     * 
+     * @param \app\Http\Requests\UserRequest $request O objeto de requisição contendo os dados do usuário aser criado.
+     * @return \illuminate\Http\JsonResponse
+     */
+    public function store(UserRequest $request): JsonResponse
     {
         // Iniciar a transação
         DB::beginTransaction();
 
-        try
-        {
+        try {
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
@@ -68,24 +72,93 @@ class UserController extends Controller
             // Operação é concluida 
             DB::commit();
 
-            return response() -> json([
+            // Retorna os dados do usuário criado e uma mensagem de sucesso com status 201
+            return response()->json([
                 'starus' => true,
                 'user' => $user,
                 'message' => "Usuário cadastrado com sucesso!",
             ], 201);
+        } catch (Exception $e) {
+
+            // Operação não é cnocluida com êxito
+            DB::rollBack();
+
+            // Retorna uma mensagem de erro com status 400
+            return response()->json([
+                'starus' => false,
+                'message' => "Usuário não cadastrado!",
+            ], 400);
         }
-        catch(Exception $e)
-        {
+    }
+
+
+    /**
+     * Atualizar os dados de um usuário existente com base nos dados fornecidos na reuisição.
+     * 
+     * @param \App\Http\Requests\UserRequest $request O objeto de requisição contendo os dados do usuário a ser atualizado.
+     * @param \App\Models\User $user O usuário a ser autalizado.
+     * @return \illuminate\Http\JsonResponse
+     */
+    public function update(UserRequest $request, User $user): JsonResponse
+    {
+        // Iniciar a transação
+        DB::beginTransaction();
+
+        try {
+
+            // Editar o registro no banco de dados
+            $user->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            // Operação é concluida 
+            DB::commit();
+
+            // Retorna os dados do usuário editado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'starus' => true,
+                'user' => $user,
+                'message' => "Usuário editado com sucesso!",
+            ], 200);
+        } catch (Exception $e) {
 
             // Operação não é cnocluida com êxito
             DB::rollBack();
 
             // Retorna uma mensagem de erro com statua 400
-            return response() -> json([
+            return response()->json([
                 'starus' => false,
-                'message' => "Usuário não cadastrado!",
+                'message' => "Usuário não editado!",
             ], 400);
+        }
+    }
 
+    /**
+     * Excluir usuário no banco de dados.
+     * 
+     * @param  \App\Models\User  $user O usuário a ser excluído.
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        try {
+            // Apagar o regitro no banco de dados
+            $user->delete();
+
+            // Retorna os dados do usuário editado e uma mensagem de sucesso com status 200
+            return response()->json([
+                'starus' => true,
+                'user' => $user,
+                'message' => "Usuário editado com sucesso!",
+            ], 200);
+        } catch (Exception $e) {
+            // Retorna uma mensagem de erro com statua 400
+            return response()->json([
+                'starus' => false,
+                'message' => "Usuário não apagado!",
+            ], 400);
         }
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserRequest extends FormRequest
 {
@@ -15,16 +17,54 @@ class UserRequest extends FormRequest
     }
 
     /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * Manipular falha de validação e retornar uma resposta JSON com os erros de validação.
+     * 
+     * @param \illuminate\Contracts\Validation\Validator $validator o objeto de validação que contém os erros de validação.
+     * @throws \iluminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'erros' => $validator->errors(),
+        ], 422));// O codigo de status HTTP 422 significa "Unprocessable Entity" (Entidade Não
+        // Processavel). Esse código é usando quando o servidor entende a requisição do cliente, mas não pode
+        // processá-la devido a erros de lidação no lado do servidor. 
+    }
+    /**
+     * Retorna as regras de vlaidação para os dados do usuário.
+     * 
+     * @return arry<string, \illuminate\Contracts\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
+
+        // Recuperar o id do usuario enviado na URL
+        $userId = $this->route('user');
+
         return [
             'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
+            'email' => 'required | email | unique:users,email' . ($userId ? $userId->id : null),
+            'password' => 'required|min:6'
+        ];
+    
+    }
+
+    /**
+     * Retorna as mensagens de erro personalizadas para as regras de validação.
+     * 
+     * @return array
+     */
+    public function messages(): array
+    {
+        return[
+            'name.required' => 'Campo nome é obrigatório!',
+            'email.required' => 'Campo e-mail é obrigatório!',
+            'email.email' => 'Necessario enviar e-mail válido!',
+            'email.unique' => 'O e-mail ja está cadastrado',
+            'password.required' => 'Campo senha é obrigatório!',
+            'password.min' => 'Senha com no mínimo :min caracteres!'
         ];
     }
 }
+
